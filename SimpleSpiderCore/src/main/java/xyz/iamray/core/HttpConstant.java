6714fc177164ce5870a6b2456b627f5f65c61e08
ray <923908153@qq.com>
2020-02-05 18:37:05 +0800

    添加Http请求头的相关常量

---
+++ SimpleSpiderCore/src/main/java/xyz/iamray/core/HttpConstant.java
@@ -0,0 +1,38 @@
+package xyz.iamray.core;
+
+/**
+ * @author liuwenrui
+ * @date 2018/5/26
+ */
+public abstract class HttpConstant {
+    public static abstract class Method {
+
+        public static final String GET = "GET";
+
+        public static final String HEAD = "HEAD";
+
+        public static final String POST = "POST";
+
+        public static final String PUT = "PUT";
+
+        public static final String DELETE = "DELETE";
+
+        public static final String TRACE = "TRACE";
+
+        public static final String CONNECT = "CONNECT";
+
+    }
+
+    public static abstract class StatusCode {
+
+        public static final int CODE_200 = 200;
+
+    }
+
+    public static abstract class Header {
+
+        public static final String REFERER = "Referer";
+
+        public static final String USER_AGENT = "User-Agent";
+    }
+}
